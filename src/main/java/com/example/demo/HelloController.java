5583e350e32cb5f66f5c6f503ff619b58f776344
Fahad Ali Khan <fk0881889@gmail.com>
2024-09-07 13:24:00 +0530

    Added code

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, Spring!";
+    @GetMapping("/buggy")
+    public String getResult() {
+        int result = 10 / 0;
+        return "This will never be reached";
+    }
+
+    @GetMapping("/nullpointer")
+    public String GetMessage() {
+        String message = null;
+        return message.toUpperCase();
     }
 }
